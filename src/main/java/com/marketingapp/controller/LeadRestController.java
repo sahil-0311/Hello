b3f2b822556dacb48a8cbb1e8e5ed59ac4083da4
sahil-0311 <sahilpandey0311@gmail.com>
2022-11-10 16:16:34 +0530

    backup1

---
+++ src/main/java/com/marketingapp/controller/LeadRestController.java
@@ -0,0 +1,47 @@
+package com.marketingapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marketingapp.entities.Lead;
+import com.marketingapp.repository.LeadRepository;
+
+@RestController
+@RequestMapping("/api/leads")
+public class LeadRestController {
+
+	@Autowired
+	private LeadRepository leadRepo;
+
+    
+	@GetMapping
+    public List<Lead> getAllLeads(){
+		List<Lead> leads = leadRepo.findAll();
+	    return leads;
+	}
+     
+	@PostMapping
+	public void saveLead(@RequestBody Lead lead) {
+		leadRepo.save(lead);
+	}
+	
+	@PutMapping
+	public void updateLead(@RequestBody Lead lead) {
+		leadRepo.save(lead);
+	}
+	//http://localhost:8080/api/leads/delete/29
+	@DeleteMapping("/delete/{id}")
+	public void deleteLead(@PathVariable("id") long id) {
+		leadRepo.deleteById(id);
+	}
+
+}
---
+++ src/main/java/com/marketingapp/util/EmailService.java
@@ -0,0 +1,7 @@
+package com.marketingapp.util;
+
+public interface EmailService {
+
+	
+	public void sendSimpleMail(String to, String sub, String emailBody);
+}
